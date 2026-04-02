ef8f9e1af37d9ca94e8bc744b9bbd0e16f839a55
Emmanuel Awoyemi <150502155+EAwoyemi110@users.noreply.github.com>
2026-04-01 22:47:05 -0500

    Added set dept method
    Made it general but might change that later to be more specific

--- Email-admin-app/Main/emailapp/src/Email.java
+++ Email-admin-app/Main/emailapp/src/Email.java
@@ -1,26 +1,45 @@
-package EmailApp;
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
-
     private String lastName;
-
     private String password;
-
     private String department = "Fashion";
-
     private int mailboxCapacity;
-
+    private int passwordDefaultLength;
     private String altEmail;
 
-    //Access through class APIs
-
-    //Constructor to recieve first and last name
-    //Dept set
-    //Generate random password
-    //Set Alt email
-    //Change password
-    public Email (String firstName, String lastName) {
+    // Constructor to receive first and last name
+    public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
+        this.department = setDept();
+        System.out.println("Your department is " + this.department);
+
     }
+
+    // Call method setting up their department email and return their new email
+    private String setDept() {
+        System.out.print("DEPT CODES\n1 for sales\n2 for development\n3 for accounting\n0 for none\n");
+        Scanner in = new Scanner(System.in);
+        int departmentChoice = in.nextInt();
+
+
+        if (departmentChoice == 1) {
+            return "Sales";
+        } else if (departmentChoice == 2) {
+            return "Development";
+        } else if (departmentChoice == 3) {
+            return "Accounting";
+        } else if (departmentChoice == 0) {
+            return "None";
+        } else {
+            return "Please enter a valid number.";
+        }
+    }
+
+
 }
+//Generate random number and treat password as an array and use number as the index
